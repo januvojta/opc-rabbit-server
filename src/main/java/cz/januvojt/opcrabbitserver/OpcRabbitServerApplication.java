b600dd9eefc4cbc5a413bffd88b25c926ecc8235
Vojta <januvojta@gmail.com>
2022-04-19 20:19:06 +0200

    init

---
+++ src/main/java/cz/januvojt/opcrabbitserver/OpcRabbitServerApplication.java
@@ -0,0 +1,13 @@
+package cz.januvojt.opcrabbitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpcRabbitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpcRabbitServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/januvojt/opcrabbitserver/OpcRabbitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.januvojt.opcrabbitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpcRabbitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
